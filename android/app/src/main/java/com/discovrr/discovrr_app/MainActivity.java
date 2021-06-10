58302d176595c643d33ba517ae21e201af1e3e09
sudoersfile <tahscenery@gmail.com>
2021-06-10 20:27:31 +1000

    Rename Android project

---
+++ android/app/src/main/java/com/discovrr/discovrr_app/MainActivity.java
@@ -0,0 +1,35 @@
+package com.discovrr.discovrr_app;
+
+import android.os.Bundle;
+import com.facebook.react.ReactActivity;
+import com.facebook.react.ReactActivityDelegate;
+import com.facebook.react.ReactRootView;
+import com.swmansion.gesturehandler.react.RNGestureHandlerEnabledRootView;
+import com.zoontek.rnbootsplash.RNBootSplash;
+
+public class MainActivity extends ReactActivity {
+    /**
+     * Returns the name of the main component registered from JavaScript. This is used to schedule
+     * rendering of the component.
+     */
+    @Override
+    protected String getMainComponentName() {
+        return "discovrr";
+    }
+
+    @Override
+    protected ReactActivityDelegate createReactActivityDelegate() {
+        return new ReactActivityDelegate(this, getMainComponentName()) {
+            @Override
+            protected ReactRootView createRootView() {
+                return new RNGestureHandlerEnabledRootView(MainActivity.this);
+            }
+        };
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(null);
+        RNBootSplash.init(R.drawable.bootsplash, MainActivity.this);
+    }
+}
