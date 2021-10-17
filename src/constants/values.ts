0b786ad1df4201a2a8c35e27a00dd07dd754d73a
sudoersfile <tahscenery@gmail.com>
2021-10-17 11:32:28 +1100

    Upgrade marketing version to `3.0.0`

--- src/constants/values.ts
+++ src/constants/values.ts
@@ -8,5 +8,5 @@ export const DEFAULT_MIN_BOTTOM_TAB_BAR_HEIGHT = 55;
 
 export const DEFAULT_ICON_LIKE_ANIMATION: Animation = 'tada';
 
-export const APP_VERSION = `${getVersion()}.3`;
-export const APP_BUILD = '2021.10.15.00';
+export const APP_VERSION = `${getVersion()}.0`;
+export const APP_BUILD = '2021.10.17.00';
