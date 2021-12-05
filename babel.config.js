9e597630479b4b3d861d6f6d549f6ed82273acea
sudoersfile <tahscenery@gmail.com>
2021-12-05 13:44:07 +1100

    Add `transform-remove-console` babel plugin

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
   plugins: [
+    ['transform-remove-console'],
     ['react-native-reanimated/plugin'],
     [
       require.resolve('babel-plugin-module-resolver'),
