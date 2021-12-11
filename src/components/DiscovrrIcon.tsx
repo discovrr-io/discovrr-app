c471eb51fe11f968b0ee769b4ac15f789089c3ad
sudoersfile <tahscenery@gmail.com>
2021-12-11 20:04:54 +1100

    Update Discovrr icon font

--- src/components/DiscovrrIcon.tsx
+++ src/components/DiscovrrIcon.tsx
@@ -2,14 +2,14 @@ import * as React from 'react';
 import { createIconSetFromFontello } from 'react-native-vector-icons';
 import { IconProps } from 'react-native-vector-icons/Icon';
 
-const DiscovrrIconSet = createIconSetFromFontello(
+const DiscovrrIconsSet = createIconSetFromFontello(
   require('../../assets/fonts/discovrr-icons-font-config.json'),
-  'fontello',
+  'discovrr-icons',
   'DiscovrrIcons.ttf',
 );
 
 type DiscovrrIconProps = Omit<IconProps, 'name'>;
 
 export default function DiscovrrIcon(props: DiscovrrIconProps) {
-  return <DiscovrrIconSet name="discovrr-logo" {...props} />;
+  return <DiscovrrIconsSet name="logo" {...props} />;
 }
