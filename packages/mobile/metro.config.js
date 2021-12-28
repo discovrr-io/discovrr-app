5a3967f1ccebcacf882d044889225460bef6bf5a
Ta-Seen Islam <taseen00.islam@gmail.com>
2021-12-28 23:21:55 +1100

    Set up monorepo

---
+++ packages/mobile/metro.config.js
@@ -0,0 +1,44 @@
+const exclusionList = require('metro-config/src/defaults/exclusionList');
+const {
+  getMetroTools,
+  getMetroAndroidAssetsResolutionFix,
+} = require('react-native-monorepo-tools');
+
+const monorepoMetroTools = getMetroTools();
+const androidAssetsResolutionFix = getMetroAndroidAssetsResolutionFix();
+
+/**
+ * Metro configuration for React Native
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+/*
+ * Monorepo support copied from:
+ * https://github.com/mmazzarolo/react-native-universal-monorepo/blob/3efafc58f247c83b1a83469dc7fb579143c18b86/packages/mobile/metro.config.js
+ */
+
+module.exports = {
+  transformer: {
+    getTransformOptions: async () => ({
+      transform: {
+        experimentalImportSupport: false,
+        inlineRequires: false,
+      },
+    }),
+  },
+  server: {
+    enhanceMiddleware: middleware => {
+      return androidAssetsResolutionFix.applyMiddleware(middleware);
+    },
+  },
+  // Add additional Yarn workspace package roots to the module map.
+  // This allows importing from all the project's packages.
+  watchFolders: monorepoMetroTools.watchFolders,
+  resolver: {
+    // Ensure we resolve nohoist libraries from this directory.
+    blockList: exclusionList(monorepoMetroTools.blockList),
+    extraNodeModules: monorepoMetroTools.extraNodeModules,
+  },
+};
